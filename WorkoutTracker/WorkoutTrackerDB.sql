d4b668ce231565fb6848da9068544b4674b6e080
ccrosby1 <153571435+ccrosby1@users.noreply.github.com>
2025-11-03 19:27:42 -0700

    Added navbar and registration

--- WorkoutTracker/WorkoutTrackerDB.sql
+++ WorkoutTracker/WorkoutTrackerDB.sql
@@ -3,7 +3,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema workouttracker
 -- -----------------------------------------------------
@@ -15,18 +14,44 @@ CREATE SCHEMA IF NOT EXISTS `workouttracker` DEFAULT CHARACTER SET utf8 ;
 USE `workouttracker` ;
 
 -- -----------------------------------------------------
--- Table `workouttracker`.`workout`
+-- Table `users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `workouttracker`.`workout` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATETIME NULL,
-  `name` VARCHAR(45) NULL,
-  `duration` INT NULL,
-  `muscleGroup` VARCHAR(100) NULL,
-  `notes` TEXT(1000) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `email` VARCHAR(100) NULL,
+  `password` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `id_UNIQUE` (`user_id` ASC))
 ENGINE = InnoDB;
+
+-- Insert default admin user
+INSERT INTO `users` (`username`, `email`, `password`)
+VALUES ('admin', 'admin@test.com', 'admin');
+
+USE `workouttracker` ;
+
+-- -----------------------------------------------------
+-- Table `workout`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `workout` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NULL DEFAULT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `duration` INT(11) NULL DEFAULT NULL,
+  `muscleGroup` VARCHAR(100) NULL DEFAULT NULL,
+  `notes` TEXT NULL DEFAULT NULL,
+  `users_user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_workout_users_idx` (`users_user_id` ASC),
+  CONSTRAINT `fk_workout_users`
+    FOREIGN KEY (`users_user_id`)
+    REFERENCES `mydb`.`users` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
