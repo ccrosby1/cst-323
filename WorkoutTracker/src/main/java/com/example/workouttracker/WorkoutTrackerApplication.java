58146112873334018911531fabf5679ff9d69591
ccrosby1 <153571435+ccrosby1@users.noreply.github.com>
2025-11-09 21:38:39 -0700

    Comments

--- WorkoutTracker/src/main/java/com/example/workouttracker/WorkoutTrackerApplication.java
+++ WorkoutTracker/src/main/java/com/example/workouttracker/WorkoutTrackerApplication.java
@@ -1,3 +1,10 @@
+/**
+ * Main application class for the Workout Tracker application.
+ * 
+ * Cody Crosby & Daniel Hanson
+ * CST-323: Cloud Computing
+ * GCU 2025
+ */
 package com.example.workouttracker;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkoutTrackerApplication {
 
+	/**
+	 * Main method to run the Spring Boot application
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutTrackerApplication.class, args);
 	}
